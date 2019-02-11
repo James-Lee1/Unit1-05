66395560c13beda2f1e401ba145183ad320e4777
James Sanii <james@ip-172-31-8-86.ca-central-1.compute.internal>
2019-02-11 19:29:52 +0000

    Unit 1 - 05 done in Java

---
+++ DiceRoll.java
@@ -0,0 +1,54 @@
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: Feb 2019
+ * Created for: ICS4U
+ * This program plays a game, where the program chooses a number from 1 to 6
+ * and the user tries to guess what the number is
+ *
+ ****************************************************************************/
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class DiceRoll {
+    public static void main(String[] args) {
+
+    Random rand = new Random();
+    Scanner userInput = new Scanner(System.in);
+
+    // Variables
+
+    // This will give a range from 0 - 5
+    int randomNumber = rand.nextInt(6);
+    int userGuess;
+    int counter;
+    
+    //Add one so that the range is from 1 - 6 
+    randomNumber += 1;
+    counter = 0;
+
+    // Asks the user what the number is
+    System.out.print("What is the secret number? (Range of 1 - 6) : ");
+    userGuess = userInput.nextInt();
+
+    // Repeats this process as long as the answer is wrong
+    while (randomNumber != userGuess) {
+
+        // One more trial that you took is added to the total
+        counter += 1;
+
+        System.out.print("Sike! THAT'S THE WRONG NUMBER");
+
+        System.out.print(" What is the secret number? (Range of 1 - 6) : ");
+        userGuess = userInput.nextInt();
+
+        }
+
+    // One more trial that you took is added to the total
+    counter += 1;
+    System.out.print("Congratulations! That is the right number!");
+    System.out.print(" And it only took " + counter + " tries. ");
+
+    }
+}
